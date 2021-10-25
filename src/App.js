9d7a22dd15831461486e8a13ef1a32670eebf6c9
ADRIAN TMOCALIUC <adriantmocaliuc@gmail.com>
2021-10-25 19:36:34 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import { Profile } from "./components";
+function App() {
+  return (
+    <>
+      <div>Hello World!!!</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export const Profile = ({name}) => {
+    return (
+        <div>Hello World !!!</div>
+    )
+}
