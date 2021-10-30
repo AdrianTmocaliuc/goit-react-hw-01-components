3f2fb5f029d9c9c5231e6ffa0741282bba884edb
ADRIAN TMOCALIUC <adriantmocaliuc@gmail.com>
2021-10-30 19:13:13 +0300

    AddAppJs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import {
   Profile,
-  Statistics,
+  // Statistics,
+  Test as Test2,
   FriendList,
   TransactionHistory,
 } from "./components/";
+import Dkjfhalhdf from "./components";
 import user from "./data/user.json";
 import statisticalData from "./data/statistical-data.json";
 import friends from "./data/friends.json";
@@ -14,8 +16,10 @@ function App() {
   return (
     <>
       <Profile user={user} />
-      <Statistics title="Upload stats" stats={statisticalData} />;
-      <Statistics stats={statisticalData} />
+      {/* <Dkjfhalhdf title="Upload stats" stats={statisticalData} />;
+      <Dkjfhalhdf stats={statisticalData} /> */}
+      <Test2 title="Upload stats" stats={statisticalData} />;
+      <Test2 stats={statisticalData} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </>
