4dfe0d9e5ccf6ba1bfa77fcea8ce234eac1f6fc6
ADRIAN TMOCALIUC <adriantmocaliuc@gmail.com>
2021-11-02 20:58:03 +0200

    somechabge

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,4 +1,4 @@
-import PropTypes from "prop-types";
+import { PropTypes } from "prop-types";
 
 import s from "./Statistics.module.css";
 import { getRandomHexColor } from "../../utils/randomColor";
